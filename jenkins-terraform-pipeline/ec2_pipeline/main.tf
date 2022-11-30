0b5c41f1812d35a011ee29142f43aa76de72e95d
sim1501 <96864975+sim1501@users.noreply.github.com>
2022-12-01 01:30:51 +0530

    updating access_key and secret_key

--- jenkins-terraform-pipeline/ec2_pipeline/main.tf
+++ jenkins-terraform-pipeline/ec2_pipeline/main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 provider "aws" {
   region = "us-west-2"
-  access_key = "AKIATYGL3F3YSOT66FOX"
-  secret_key = "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"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 resource "aws_instance" "my_terraform_instance" {
