8c717b5ea881b9ba7ca779ace7d8fea34415a95a
sim1501 <96864975+sim1501@users.noreply.github.com>
2022-12-01 01:13:35 +0530

    Update main.tf

--- jenkins-terraform-pipeline/ec2_pipeline/main.tf
+++ jenkins-terraform-pipeline/ec2_pipeline/main.tf
@@ -15,9 +15,4 @@ provider "aws" {
 resource "aws_instance" "my_terraform_instance" {
   ami           = var.ami
   instance_type = var.instance_type
-
-  network_interface {
-    network_interface_id = var.network_interface_id
-    device_index         = 0
-  }
 }
