73e65d918457db1b6588a027e081c3bf5719a51d
sim1501 <96864975+sim1501@users.noreply.github.com>
2022-12-01 01:09:52 +0530

    Update main.tf

--- jenkins-terraform-pipeline/ec2_pipeline/main.tf
+++ jenkins-terraform-pipeline/ec2_pipeline/main.tf
@@ -8,6 +8,8 @@ terraform {
 }
 provider "aws" {
   region = "us-west-2"
+  access_key = "AKIATYGL3F3YSOT66FOX"
+  secret_key = "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"
 }
 
 resource "aws_instance" "my_terraform_instance" {
